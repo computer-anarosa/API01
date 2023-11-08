10f951fe9980079ab1f1a8a909b343a0bc7dad33
Ana Rosa <dosanarosa03@gmail.com>
2023-11-08 19:57:24 -0300

    por agora

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # API01
  Minha 1ª API REST em Python.
+
+## Requisitos
+
+- Python 3.10^
---
+++ api01.py
@@ -0,0 +1,18 @@
+items = [
+    {
+        "id": 1,
+        "nome": "Bagulho",
+        "description": "Apena um bagulho",
+        "location": "Em uma caixa"
+    }, {
+        "id": 2,
+        "name": "Tranqueira",
+        "description": "Apenas uma tranqueira qualquer",
+        "location": "Em um gaveteiro"
+    }, {
+        "id": 3,
+        "name": "Bagulete",
+        "description": "Um bagulete qualquer",
+        "location": "Na esquina"
+    }
+]
